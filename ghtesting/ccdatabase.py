e9f9abd0405332fe6834685f00579a3c27df4608
Muhammad Haroon <m_haroon96@hotmail.com>
2021-11-26 19:00:16 -0800

    added codecov api code

---
+++ ghtesting/ccdatabase.py
@@ -0,0 +1,35 @@
+import pymongo
+import logging as log
+
+class CCDatabase:
+    def __init__(self, db: str, collection: str, connectionString="mongodb://localhost:27017/"):
+        self.client = pymongo.MongoClient(connectionString)
+        self.db = self.client[db]
+        if collection not in self.db.list_collection_names():
+            self.db.create_collection(collection, capped=False) # unlimited documents
+        self.col = self.db[collection]
+
+    def update_repo(self, repojson):
+        self.col.update_one({'_id': repojson['_id']}
+                            , {'$set': repojson}
+                            , upsert=True)
+
+    def append_to_repo(self, name, jsondata):
+        self.col.update_one({'_id': name}
+                            , {'$set': jsondata}
+                            , upsert=True)
+
+    def repos_count(self):
+        return self.col.count_documents({})
+
+    def get_repos_by_name(self, name):
+        return self.col.find({'_id': name})[0]
+
+    def get_repos(self):
+        return self.col.find({}, {'_id': 1, 'data': 1})
+    
+    def get_commits(self, _id):
+        return self.col.find({'_id': _id}, {'_id': 1, 'commits': 1})
+
+    def drop_database(self, dbname):
+        self.client.drop_database(dbname)
